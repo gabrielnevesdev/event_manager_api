757df0ae701089221aa55ff711361ece26b72d20
Gabriel Santos <gabriel.santos@tmaior.com.br>
2023-11-11 22:38:08 -0300

    feat: implement tickets_sold controller

--- src/tickets_sold/tickets_sold.module.ts
+++ src/tickets_sold/tickets_sold.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TicketsSoldService } from './tickets_sold.service';
 import { TicketsSoldController } from './tickets_sold.controller';
+import { TicketsSold } from './entities/tickets_sold.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TicketsSold])],
   controllers: [TicketsSoldController],
   providers: [TicketsSoldService],
 })
