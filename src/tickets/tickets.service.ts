61ae395f1b85c7ac008ede6f6e9c712854dbc203
Gabriel Santos <gabriel.santos@tmaior.com.br>
2023-11-11 22:37:40 -0300

    feat: implement ticket controller

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
+import { Ticket } from './entities/ticket.entity';
 
 @Injectable()
 export class TicketsService {
-  create(createTicketDto: CreateTicketDto) {
-    return 'This action adds a new ticket';
+  constructor(
+    @InjectRepository(Ticket)
+    private readonly repository: Repository<Ticket>,
+  ) {}
+
+  create(createTicketDto: CreateTicketDto): Promise<Ticket> {
+    const ticket = this.repository.create(createTicketDto);
+    return this.repository.save(ticket);
   }
 
-  findAll() {
-    return `This action returns all tickets`;
+  findAll(): Promise<Ticket[]> {
+    return this.repository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ticket`;
+  findOne(id: string): Promise<Ticket> {
+    return this.repository.findOne({ where: { id } });
   }
 
-  update(id: number, updateTicketDto: UpdateTicketDto) {
-    return `This action updates a #${id} ticket`;
+  async update(id: string, updateTicketDto: UpdateTicketDto): Promise<Ticket> {
+    const ticket = await this.repository.preload({
+      id: id,
+      ...updateTicketDto,
+    });
+    if (!ticket) {
+      throw new NotFoundException(`Ticket ${id} not found`);
+    }
+    return this.repository.save(ticket);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} ticket`;
+  async remove(id: string) {
+    const ticket = await this.findOne(id);
+    return this.repository.remove(ticket);
   }
 }
