b4ae4971f1c12ac87f13aa982692d3272eb88654
Gabriel Santos <gabriel.santos@tmaior.com.br>
2023-11-13 02:34:38 -0300

    feat: add tickets_sold documentation

--- src/tickets_sold/dto/create-tickets_sold.dto.ts
+++ src/tickets_sold/dto/create-tickets_sold.dto.ts
@@ -5,20 +5,43 @@ import {
   IsNotEmpty,
   IsNumber,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTicketsSoldDto {
   @IsOptional()
   @IsUUID()
+  @ApiProperty({
+    description: 'Ticket sold id (do not pass in the request body)',
+    type: 'string',
+    format: 'uuid',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
   id: string;
 
+  @ApiProperty({
+    description: 'Quantity purchased',
+    type: 'number',
+    example: 2,
+  })
   @IsNotEmpty()
   @IsInt()
   quantity_purchased: number;
 
+  @ApiProperty({
+    description: 'Total Payment (do not pass in the request body)',
+    type: 'number',
+    example: 20.0,
+  })
   @IsOptional()
   @IsNumber()
   total_payment: number;
 
+  @ApiProperty({
+    description: 'Ticket id associated with the sale',
+    type: 'string',
+    format: 'uuid',
+    example: '550e8400-e29b-41d4-a716-446655440001',
+  })
   @IsNotEmpty()
   @IsUUID()
   ticketId: string;
--- src/tickets_sold/tickets_sold.controller.ts
+++ src/tickets_sold/tickets_sold.controller.ts
@@ -12,13 +12,31 @@ import { TicketsSoldService } from './tickets_sold.service';
 import { CreateTicketsSoldDto } from './dto/create-tickets_sold.dto';
 import { AuthGuard } from '../auth/auth.guard';
 import { JwtPayload } from 'jsonwebtoken';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiQuery,
+  ApiBody,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Tickets Sold')
 @Controller('tickets_sold')
 export class TicketsSoldController {
   constructor(private readonly ticketsSoldService: TicketsSoldService) {}
 
   @UseGuards(AuthGuard)
   @Post()
+  @ApiOperation({ summary: 'Create a new ticket sold' })
+  @ApiBody({ type: CreateTicketsSoldDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    type: CreateTicketsSoldDto,
+  })
   create(
     @Body() createTicketsSoldDto: CreateTicketsSoldDto,
     @Request() req: JwtPayload,
@@ -28,6 +46,14 @@ export class TicketsSoldController {
 
   @UseGuards(AuthGuard)
   @Get()
+  @ApiOperation({ summary: 'List all tickets sold' })
+  @ApiQuery({ name: 'page', type: Number, required: false, example: 1 })
+  @ApiQuery({ name: 'pageSize', type: Number, required: false, example: 10 })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    type: [CreateTicketsSoldDto],
+  })
   findAll(
     @Query('page') page: number = 1,
     @Query('pageSize') pageSize: number = 10,
@@ -38,6 +64,13 @@ export class TicketsSoldController {
 
   @UseGuards(AuthGuard)
   @Get(':id')
+  @ApiOperation({ summary: 'Get a ticket sold by id' })
+  @ApiParam({ name: 'id', description: 'Ticket Sold id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    type: CreateTicketsSoldDto,
+  })
   findOne(@Param('id') id: string) {
     return this.ticketsSoldService.findOne(id);
   }
