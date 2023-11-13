b5a4182026ac9bbb6cc9f96d0c31bd96032cdc21
Gabriel Santos <gabriel.santos@tmaior.com.br>
2023-11-13 02:33:56 -0300

    feat: add user_profile documentation

--- src/user_profile/user_profile.controller.ts
+++ src/user_profile/user_profile.controller.ts
@@ -9,18 +9,30 @@ import {
   UseGuards,
   Request,
 } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { UserProfileService } from './user_profile.service';
 import { CreateUserProfileDto } from './dto/create-user_profile.dto';
 import { UpdateUserProfileDto } from './dto/update-user_profile.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { JwtPayload } from 'jsonwebtoken';
 
+@ApiBearerAuth()
+@ApiTags('User Profiles')
 @Controller('user_profile')
 export class UserProfileController {
   constructor(private readonly userProfileService: UserProfileService) {}
 
   @UseGuards(AuthGuard)
   @Post()
+  @ApiOperation({ summary: 'Create a new user profile' })
+  @ApiBody({ type: CreateUserProfileDto })
   create(
     @Body() createUserProfileDto: CreateUserProfileDto,
     @Request() req: JwtPayload,
@@ -30,18 +42,39 @@ export class UserProfileController {
 
   @UseGuards(AuthGuard)
   @Get()
+  @ApiOperation({ summary: 'List all user profiles' })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    type: [CreateUserProfileDto],
+  })
   findAll() {
     return this.userProfileService.findAll();
   }
 
   @UseGuards(AuthGuard)
   @Get(':id')
+  @ApiOperation({ summary: 'Get an user profile by id' })
+  @ApiParam({ name: 'id', description: 'User Profile id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    type: CreateUserProfileDto,
+  })
   findOne(@Param('id') id: string) {
     return this.userProfileService.findOne(id);
   }
 
   @UseGuards(AuthGuard)
   @Patch(':id')
+  @ApiOperation({ summary: 'Update an user profile by id' })
+  @ApiParam({ name: 'id', description: 'User Profile id' })
+  @ApiBody({ type: CreateUserProfileDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    type: CreateUserProfileDto,
+  })
   update(
     @Param('id') id: string,
     @Body() updateUserProfileDto: UpdateUserProfileDto,
@@ -52,6 +85,8 @@ export class UserProfileController {
 
   @UseGuards(AuthGuard)
   @Delete(':id')
+  @ApiOperation({ summary: 'Remove an user profile by id' })
+  @ApiParam({ name: 'id', description: 'User Profile id' })
   remove(@Param('id') id: string, @Request() req: JwtPayload) {
     return this.userProfileService.remove(id, req);
   }
