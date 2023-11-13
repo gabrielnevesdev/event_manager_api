bfec3a35ee294b56d0e5baa6052ca3487e76937d
Gabriel Santos <gabriel.santos@tmaior.com.br>
2023-11-13 02:32:59 -0300

    feat: add user documentation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -6,24 +6,52 @@ import {
   IsOptional,
   IsUUID,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { UserRole } from '../../types/userInterface';
+
 export class CreateUserDto {
   @IsOptional()
   @IsUUID()
+  @ApiProperty({
+    description: 'User id (do not pass in the request body)',
+    type: 'string',
+    format: 'uuid',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
   id: string;
 
+  @ApiProperty({
+    description: 'Email is a unique field used to login',
+    type: 'string',
+    example: 'exemplo@test.com',
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description:
+      'Username generated spliting email before "@" (do not pass in the request body',
+    type: 'string',
+    example: 'exemplo@test.com -> exemplo',
+  })
   @IsOptional()
   @IsString()
   username: string;
 
+  @ApiProperty({
+    description: 'User Password',
+    type: 'string',
+  })
   @IsNotEmpty()
   @IsString()
   password: string;
 
+  @ApiProperty({
+    description: 'User role (optional)',
+    type: 'string',
+    enum: UserRole,
+  })
   @IsOptional()
   @IsEnum(UserRole, { each: true })
   role: UserRole;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,35 +9,58 @@ import {
   UseGuards,
   Request,
 } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
+import { JwtPayload } from 'jsonwebtoken';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { AuthGuard } from 'src/auth/auth.guard';
-import { JwtPayload } from 'jsonwebtoken';
+import { AuthGuard } from '../auth/auth.guard';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiBody({ type: CreateUserDto })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @UseGuards(AuthGuard)
   @Get()
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'List all user' })
+  @ApiResponse({ status: 200, description: 'success', type: [CreateUserDto] })
   findAll() {
     return this.usersService.findAll();
   }
 
   @UseGuards(AuthGuard)
   @Get(':id')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Find an user by id' })
+  @ApiParam({ name: 'id', description: 'user id' })
+  @ApiResponse({ status: 200, description: 'success', type: CreateUserDto })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @UseGuards(AuthGuard)
   @Patch(':id')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update an user by id' })
+  @ApiParam({ name: 'id', description: 'User id' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 200, description: 'success', type: CreateUserDto })
   update(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -48,6 +71,9 @@ export class UsersController {
 
   @UseGuards(AuthGuard)
   @Delete(':id')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Remove an user by Id' })
+  @ApiParam({ name: 'id', description: 'User ID' })
   remove(@Param('id') id: string, @Request() req: JwtPayload) {
     return this.usersService.remove(id, req);
   }
