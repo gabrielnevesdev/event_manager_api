8e900bf21dd19a429884f2aad77d9ce44256c9e8
Gabriel Santos <gabriel.santos@tmaior.com.br>
2023-11-13 02:35:41 -0300

    feat: add ticket documentation

--- src/tickets/dto/create-ticket.dto.ts
+++ src/tickets/dto/create-ticket.dto.ts
@@ -6,25 +6,54 @@ import {
   IsEnum,
   IsNumber,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { TicketsCategory } from 'src/types/ticketsInterface';
 
 export class CreateTicketDto {
   @IsOptional()
   @IsUUID()
+  @ApiProperty({
+    description: 'Ticket id (do not pass in the request body)',
+    type: 'string',
+    format: 'uuid',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
   id: string;
 
+  @ApiProperty({
+    description: 'Available Quantity',
+    type: 'number',
+    example: 100,
+  })
   @IsNotEmpty()
   @IsInt()
   available_quantity: number;
 
+  @ApiProperty({
+    description: 'Ticket category (optional)',
+    type: 'enum',
+    enum: TicketsCategory,
+    example: TicketsCategory.NORMAL,
+  })
   @IsOptional()
   @IsEnum(TicketsCategory, { each: true })
   ticket_category: TicketsCategory;
 
+  @ApiProperty({
+    description: 'Ticket Price',
+    type: 'number',
+    example: 29.99,
+  })
   @IsNotEmpty()
   @IsNumber()
   price: number;
 
+  @ApiProperty({
+    description: 'Event id associated with the ticket',
+    type: 'string',
+    format: 'uuid',
+    example: '550e8400-e29b-41d4-a716-446655440001',
+  })
   @IsNotEmpty()
   @IsUUID()
   eventId: string;
